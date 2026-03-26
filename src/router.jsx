a91f37da07b3112795a171ef7f6c64dab8fbb8f4
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2026-03-26 08:43:30 -0700

    Fixing issues with React routers

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,8 @@ import IntroPage from "./pages/IntroPage";
 import HomePage from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
 import ProjectsPage from "./pages/ProjectsPage";
+import ProjectPage2 from "./pages/ProjectPage2";
+import ProjectPage3 from "./pages/ProjectPage3";
 
 
 
@@ -13,7 +15,9 @@ export default function Router() {
         <Route path="/" element={<IntroPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/home/:id" element={<ProjectPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/project2/:id" element={<ProjectPage2 />} />
+        <Route path="/project3/:id" element={<ProjectPage3 />} />
+        {/* <Route path="/projects" element={<ProjectsPage />} /> */}
         {/* <Route path="/contact" element={<ContactPage />} /> */}
       </Routes>
     </BrowserRouter>
